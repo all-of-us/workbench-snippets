6cfd8cd25100e5d55726b85dd0612eec66756887
CH Albach <calbach@google.com>
2019-05-15 11:48:13 -0700

    AoU --> All of Us in user-facing text

--- snippets/number_of_participants_with_med_conditions.sql
+++ snippets/number_of_participants_with_med_conditions.sql
@@ -1,8 +1,7 @@
 
--- Compute the count of all unique participants in AoU
+-- Compute the count of all unique participants in All of Us
 -- that have at least one condition.
 SELECT
   COUNT(DISTINCT person_id) AS number_of_participants_with_med_conditions
 FROM
   `{DATASET}.condition_occurrence`
-
