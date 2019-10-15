084eab306c5803b9ddab867fd4626d34ce20318b
Nicole Deflaux <deflaux@google.com>
2019-10-15 15:06:05 -0700

    By default, profile up to the first 10,000 rows.

--- dataset-snippets/summarize_a_dataframe.py
+++ dataset-snippets/summarize_a_dataframe.py
@@ -4,4 +4,5 @@
 
 
 ## -----[ CHANGE THE DATAFRAME NAME(S) TO MATCH YOURS FROM DATASET BUILDER] -----
-YOUR_DATASET_NAME_person_df.profile_report()
+YOUR_DATASET_NAME_person_df.loc[:10000,:].profile_report()  # Examine up to the first 10,000 rows. Larger
+                                                            # dataframes can be profiled, but it takes more time.
