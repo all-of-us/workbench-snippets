01af3854b7f08afdb38b896e03391d9586d01da9
rfrancis1 <57016786+rfrancis1@users.noreply.github.com>
2019-11-11 15:25:46 -0600

    Add age snippets (#35)
    * Create snippet to add age to demographics dataframe from dataset builder
    
    * Simplify age calculation for R version of age snippet
    
    * Replace the operator %<>% with <- in age snippet
    
    * Add a new line at the end of R age-snippet
    
    * Add yaml references for age snippets
    
    * Add a new line at the end of python age snippet

---
+++ dataset-snippets/add_age_to_demographics.R
@@ -0,0 +1,11 @@
+# Use snippet 'add_age_to_demographics' to calculate the age of people in your demographics.
+# It assumes the 'Setup' snippet has been executed.
+# It also assumes that you got your demographics dataframe from Dataset Builder
+
+# Note: This snippet calculates current age and does not take into account whether the person is already dead
+
+
+## -----[ CHANGE THE DATAFRAME NAME(S) `YOUR_DATASET_NAME_person_df` TO MATCH YOURS FROM DATASET BUILDER] -----
+YOUR_DATASET_NAME_person_df <- YOUR_DATASET_NAME_person_df %>%
+                mutate_if(is.list, as.character) %>% 
+                mutate(AGE = year(today()) - year(YOUR_DATASET_NAME_person_df$DATE_OF_BIRTH))
---
+++ dataset-snippets/add_age_to_demographics.py
@@ -0,0 +1,9 @@
+# Use snippet 'add_age_to_demographics' to calculate the age of people in your demographics.
+# It assumes the 'Setup' snippet has been executed.
+# It also assumes that you got your demographics dataframe from Dataset Builder
+
+# Note: This snippet calculates current age and does not take into account whether the person is already dead
+
+
+## -----[ CHANGE THE DATAFRAME NAME(S) `YOUR_DATASET_NAME_person_df` TO MATCH YOURS FROM DATASET BUILDER] -----
+YOUR_DATASET_NAME_person_df['AGE'] = pd.to_datetime('today').year - YOUR_DATASET_NAME_person_df['DATE_OF_BIRTH'].dt.year
