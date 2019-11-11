9da03a2e4d75804d6c533c04ba85d4d7ac06285d
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2019-11-11 15:10:06 -0600

    Replace the operator %<>% with <- in age snippet

--- dataset-snippets/add_age_to_demographics.R
+++ dataset-snippets/add_age_to_demographics.R
@@ -6,5 +6,6 @@
 
 
 ## -----[ CHANGE THE DATAFRAME NAME(S) `YOUR_DATASET_NAME_person_df` TO MATCH YOURS FROM DATASET BUILDER] -----
-YOUR_DATASET_NAME_person_df %<>% mutate_if(is.list, as.character) %>% 
+YOUR_DATASET_NAME_person_df <- YOUR_DATASET_NAME_person_df %>%
+                mutate_if(is.list, as.character) %>% 
                 mutate(AGE = year(today()) - year(YOUR_DATASET_NAME_person_df$DATE_OF_BIRTH))
