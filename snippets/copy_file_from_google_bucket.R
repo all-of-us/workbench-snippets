9325e9f895df431db94e2338d0207b8a8b5ffb32
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2019-12-09 09:32:01 -0600

    Simplify R codes to copy file from the bucket

--- snippets/copy_file_from_google_bucket.R
+++ snippets/copy_file_from_google_bucket.R
@@ -1,26 +1,17 @@
-if (!require('cloudml')) install.packages('cloudml')
-library(cloudml)
 library(tidyverse)
 
-copy_file_from_bucket <- function(source_filename) {
-    my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
-    
-    # copy file from bucket to the current workspace
-    gs_copy(paste0(my_bucket, '/', source_filename), source_filename)
-    
-    # read file into a dataframe
-    my_dataframe <- read_csv(source_filename)
-    cat('[INFO]', {source_filename}, 'is successfully downloaded from your google bucket.')
-    
-    return(my_dataframe)
-}
+# replace 'test.csv' with the name of the file in your google bucket (don't delete the quotation marks)
+name_of_file_in_bucket = 'test6.csv'
 
 ################ DO NOT CHANGE ANYTHING ABOVE THIS LINE ##############################
 ##########                                                           #################
 
-# replace 'test.csv' with the name of the file in your google bucket (don't delete the quotation marks)
-name_of_file_in_bucket = 'test.csv'
+# Get the bucket name
+my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
+
+# Copy the file from current workspace to the bucket
+system(paste0("gsutil cp ", my_bucket, "/", name_of_file_in_bucket, " ."), intern=T)
 
 # Load the file into a dataframe
-my_dataframe = copy_file_from_bucket(name_of_file_in_bucket)
+my_dataframe  <- read_csv(name_of_file_in_bucket)
 head(my_dataframe)
