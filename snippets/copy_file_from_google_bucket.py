f43584319b799263f68fdcd5a4c9d2de49f85942
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2019-12-09 10:39:10 -0600

    Edit name of variable from source_filename to name_of_file_in_bucket in python codes

--- snippets/copy_file_from_google_bucket.py
+++ snippets/copy_file_from_google_bucket.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 # Replace 'test.csv' with THE NAME of the file you're going to download from the bucket (don't delete the quotation marks)
-source_filename = 'test.csv'
+name_of_file_in_bucket = 'test.csv'
 
 ########################################################################
 ##
@@ -14,10 +14,10 @@ source_filename = 'test.csv'
 my_bucket = os.getenv('WORKSPACE_BUCKET')
 
 # copy csv file from the bucket to the current working space
-!gsutil cp '{my_bucket}/{source_filename}' .
+!gsutil cp '{my_bucket}/{name_of_file_in_bucket}' .
 
-print(f'[INFO] {source_filename} is successfully downloaded into your working space')
+print(f'[INFO] {name_of_file_in_bucket} is successfully downloaded into your working space')
 # save dataframe in a csv file in the same workspace as the notebook
-my_dataframe = read_csv(source_filename)
+my_dataframe = read_csv(name_of_file_in_bucket)
 my_dataframe.head()
     
