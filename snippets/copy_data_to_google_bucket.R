bcb62ab3c40e66e192a6a44a5a9b3a22560c4d53
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2019-12-09 09:23:24 -0600

    Simply R codes to copy data to the bucket

--- snippets/copy_data_to_google_bucket.R
+++ snippets/copy_data_to_google_bucket.R
@@ -1,19 +1,4 @@
-if (!require('cloudml')) install.packages('cloudml')
-library(cloudml)
 library(tidyverse)
-
-copy_data_to_google_bucket <- function(source_dataframe, filename) {
-    # store the dataframe in current workspace
-    write_excel_csv(source_dataframe, filename)
-    
-    my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
-    # copy file from current workspace to the bucket
-    gs_copy(filename, my_bucket)
-    cat('[INFO]', filename, "is successfully uploaded in your google bucket.")
-}
-
-################ DO NOT CHANGE ANYTHING ABOVE THIS LINE ##############################
-##########                                                           #################
 
 # Replace df with THE NAME OF YOUR DATAFRAME
 my_dataframe <- df
@@ -21,4 +6,17 @@ my_dataframe <- df
 # Replace 'test3.csv' with THE NAME of the file you're going to store in the bucket (don't delete the quotation marks)
 destination_filename <- 'test3.csv'
 
-copy_data_to_google_bucket(my_dataframe, destination_filename)
+################ DO NOT CHANGE ANYTHING ABOVE THIS LINE ##############################
+##########                                                           #################
+
+# store the dataframe in current workspace
+write_excel_csv(my_dataframe, destination_filename)
+
+# Get the bucket name
+my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
+
+# Copy the file from current workspace to the bucket
+system(paste0("gsutil cp ./", destination_filename, " ", my_bucket), intern=T)
+
+# Check if file is in the bucket
+system(paste0("gsutil ls ", my_bucket, "/*.csv"), intern=T)
