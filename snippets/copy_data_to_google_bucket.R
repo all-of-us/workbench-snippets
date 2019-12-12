585c6c095c028a08364369b5679dc191362cbc3b
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2019-12-11 18:29:17 -0600

    Specify a separate data folder in R codes to store data in Google Bucket

--- snippets/copy_data_to_google_bucket.R
+++ snippets/copy_data_to_google_bucket.R
@@ -4,7 +4,7 @@ library(tidyverse)
 my_dataframe <- df
 
 # Replace 'test3.csv' with THE NAME of the file you're going to store in the bucket (don't delete the quotation marks)
-destination_filename <- 'test3.csv'
+destination_filename <- 'test.csv'
 
 ########################################################################
 ##
@@ -19,7 +19,7 @@ write_excel_csv(my_dataframe, destination_filename)
 my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
 
 # Copy the file from current workspace to the bucket
-system(paste0("gsutil cp ./", destination_filename, " ", my_bucket), intern=T)
+system(paste0("gsutil cp ./", destination_filename, " ", my_bucket, "/data/"), intern=T)
 
 # Check if file is in the bucket
-system(paste0("gsutil ls ", my_bucket, "/*.csv"), intern=T)
+system(paste0("gsutil ls ", my_bucket, "/data/*.csv"), intern=T)
--- snippets/copy_file_from_google_bucket.R
+++ snippets/copy_file_from_google_bucket.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # replace 'test.csv' with the name of the file in your google bucket (don't delete the quotation marks)
-name_of_file_in_bucket <- 'test6.csv'
+name_of_file_in_bucket <- 'test.csv'
 
 ########################################################################
 ##
@@ -13,7 +13,7 @@ name_of_file_in_bucket <- 'test6.csv'
 my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
 
 # Copy the file from current workspace to the bucket
-system(paste0("gsutil cp ", my_bucket, "/", name_of_file_in_bucket, " ."), intern=T)
+system(paste0("gsutil cp ", my_bucket, "/data/", name_of_file_in_bucket, " ."), intern=T)
 
 # Load the file into a dataframe
 my_dataframe  <- read_csv(name_of_file_in_bucket)
