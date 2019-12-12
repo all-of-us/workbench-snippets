220f58dd1c7eddb37622e90adbcef62a63850fa7
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2019-12-12 09:16:57 -0600

    Edit setup for gbucket-snippets to include only the necessary libraries

--- gbucket-snippets/snippets_setup.py
+++ gbucket-snippets/snippets_setup.py
@@ -1,24 +1,5 @@
-!pip3 install --upgrade --user pandas_profiling statsmodels
-
 import os
 import numpy as np
 import pandas as pd
-import pandas_profiling
-import plotnine
-from plotnine import *  # Provides a ggplot-like interface to matplotlib.
 
-## Plot setup.
-theme_set(theme_bw(base_size = 11)) # Default theme for plots.
 
-def get_boxplot_fun_data(df):
-  """Returns a data frame with a y position and a label, for use annotating ggplot boxplots.
-
-  Args:
-    d: A data frame.
-  Returns:
-    A data frame with column y as max and column label as length.
-  """
-  d = {'y': max(df), 'label': f'N = {len(df)}'}
-  return(pd.DataFrame(data=d, index=[0]))
-
-# NOTE: if you get any errors from this cell, restart your kernel and run it again.
