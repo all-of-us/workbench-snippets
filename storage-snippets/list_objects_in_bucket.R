d7c04c9194d2c6427449c74b73145a50a84a88b5
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2020-01-31 12:52:01 -0600

    Create code to list objects in bucket

---
+++ storage-snippets/list_objects_in_bucket.R
@@ -0,0 +1,9 @@
+# This snippet assumes that you run setup first
+
+# This code lists objects in your Google Bucket
+
+# Get the bucket name
+my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
+
+# List objects in the bucket
+system(paste0("gsutil ls ", my_bucket), intern=T)
