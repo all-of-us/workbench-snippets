378942aefd816d361f7703aea0f91306afb632b0
rfrancis1 <francis.r.ratsimbazafy.1@vumc.org>
2020-04-15 14:30:42 -0500

    Create summarize_a_module snippet for python

---
+++ dataset-snippets/summarize_a_survey_module.py
@@ -0,0 +1,24 @@
+# Use snippet 'summarize_a_survey_module' to print a table of participant counts by question in a module
+# The snippet assumes that a dataframe containing survey questions and answers
+
+# Update the next 3 lines
+survey_df = None
+module_name = None # e.g: 'The Basics' (don't forget the quotes)
+denominator = None # e.g: 200000
+
+####################################################################################
+#                           DON'T CHANGE FROM HERE
+####################################################################################
+
+def summarize_a_module(df, module=None, denominator=None):
+    if module:
+        df = df[df['survey'].str.lower() == module.lower()].copy()
+    data = (df.groupby(['survey','question_concept_id','question'])['person_id'].nunique()
+                .reset_index()
+                .rename(columns={'person_id':'n_participant'}))
+    if denominator:
+        data['response_rate'] = round(100*data['n_participant']/denominator,2)
+    return data
+
+summarize_a_module(df=survey_df, module=module_name, denominator=denominator)
+
