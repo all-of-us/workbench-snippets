06cae5fa0d0541a1881f791910a18d584dd5e536
Nicole Deflaux <deflaux@google.com>
2020-07-15 12:30:08 -0700

    Add snippet for HTML snapshots.

---
+++ storage-snippets/interact_with_html_snapshots.py
@@ -0,0 +1,4 @@
+from terra_widgets.html_snapshots import display_html_snapshots_widget
+
+# This will display a user interface to interact with HTML snapshots stored in the workspace bucket.
+display_html_snapshots_widget()
