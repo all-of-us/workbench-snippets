18a7a4fb18068abe2f07401dabfbb4f89cd65d77
Nicole Deflaux <deflaux@users.noreply.github.com>
2021-01-28 12:17:48 -0800

    Remove obsolete comments.

--- storage-snippets/interact_with_html_snapshots.py
+++ storage-snippets/interact_with_html_snapshots.py
@@ -1,6 +1,4 @@
 from terra_widgets.html_snapshots import display_html_snapshots_widget
-# You may see some 'FutureWarning' messages emitted by Tensorflow and numpy. These are
-# safe to ignore and will go away the next time the base image updates its package versions.
 
 # This will display a user interface to interact with HTML snapshots stored in the workspace bucket.
 display_html_snapshots_widget()
