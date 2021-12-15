6c6623fb82a4e97d7f7cadae8150261b402d9c4b
Nicole Deflaux <deflaux@users.noreply.github.com>
2021-12-15 12:49:04 -0800

    Update to correct dataset id.

--- dataset-snippets/codegen_sample.r
+++ dataset-snippets/codegen_sample.r
@@ -70,35 +70,35 @@ AND (
 # NOTE: By default data exported multiple times on the same day will overwrite older copies.
 #       But data exported on a different days will write to a new location so that historical
 #       copies can be kept as the dataset definition is changed.
-survey_export_87319558_path <- file.path(
-    Sys.getenv("WORKSPACE_BUCKET"),
-    "bq_exports",
-    Sys.getenv("OWNER_EMAIL"),
-    strftime(lubridate::now(), "%Y%m%d"),  # Comment out this line if you want the export to always overwrite.
-    "survey_87319558",
-    "survey_87319558_*.csv")
-message(str_glue('The data will be written to {survey_export_87319558_path}. Use this path when reading ',
+survey_export_15947426_path <- file.path(
+  Sys.getenv("WORKSPACE_BUCKET"),
+  "bq_exports",
+  Sys.getenv("OWNER_EMAIL"),
+  strftime(lubridate::now(), "%Y%m%d"),  # Comment out this line if you want the export to always overwrite.
+  "survey_15947426",
+  "survey_15947426_*.csv")
+message(str_glue('The data will be written to {survey_export_15947426_path}. Use this path when reading ',
                  'the data into your notebooks in the future.'))
 
 # Perform the query and export the dataset to Cloud Storage as CSV files.
 # NOTE: You only need to run `bq_table_save` once. After that, you can
 #       just read data from the CSVs in Cloud Storage.
 bq_table_save(
-    bq_dataset_query(Sys.getenv("WORKSPACE_CDR"), dataset_87319558_survey_sql, billing = Sys.getenv("GOOGLE_PROJECT")),
-    survey_export_87319558_path,
-    destination_format = "CSV")
+  bq_dataset_query(Sys.getenv("WORKSPACE_CDR"), dataset_15947426_survey_sql, billing = Sys.getenv("GOOGLE_PROJECT")),
+  survey_export_15947426_path,
+  destination_format = "CSV")
 
 # Read the data directly from Cloud Storage into memory.
-# NOTE: Alternatively you can `gsutil -m cp {survey_export_87319558_path}` to copy these files
+# NOTE: Alternatively you can `gsutil -m cp {survey_export_15947426_path}` to copy these files
 #       to the Jupyter disk.
-dataset_87319558_survey_df <- bind_rows(
-    map(system2('gsutil', args = c('ls', survey_export_87319558_path), stdout = TRUE, stderr = TRUE),
-        function(csv) {
-            message(str_glue('Loading {csv}.'))
-            read_csv(pipe(str_glue('gsutil cat {csv}')))
-        })
-    )
+dataset_15947426_survey_df <- bind_rows(
+  map(system2('gsutil', args = c('ls', survey_export_15947426_path), stdout = TRUE, stderr = TRUE),
+      function(csv) {
+        message(str_glue('Loading {csv}.'))
+        read_csv(pipe(str_glue('gsutil cat {csv}')))
+      })
+)
 
-dim(dataset_87319558_survey_df)
+dim(dataset_15947426_survey_df)
 
-head(dataset_87319558_survey_df)
+head(dataset_15947426_survey_df)
