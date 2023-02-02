0475ace83253b776c1a682f5f12d8d17c85162ff
Neha Saxena <nsaxena@broadinstitute.org>
2023-02-02 14:29:16 -0500

    Update cromwell script

--- py/py_cromwell_setup.py
+++ py/py_cromwell_setup.py
@@ -30,7 +30,7 @@ def check_for_app(env):
             if workspace_namespace == env['workspace_namespace']:
                 return potential_app_name, potential_app_status, proxy_url['cromwell-service']
 
-    return None, None
+    return None, None, None
 
 def get_app_details(env, app_name):
     get_app_url = f'{env["leonardo_url"]}/api/google/v1/apps/{env["google_project"]}/{app_name}'
@@ -38,7 +38,8 @@ def get_app_details(env, app_name):
     r = requests.get(
         get_app_url,
         params={
-            'includeDeleted': 'true'
+            'includeDeleted': 'true',
+            'role': 'creator'
         },
         headers={
             'Authorization': f'Bearer {env["token"]}'
@@ -63,10 +64,10 @@ def configure_cromwell(env, proxy_url):
      print('Updating cromwell config')
      file = f'{str(Path.home())}/.cromshell/cromshell_config.json'
      configuration = {
-        'cromwell_server': proxy_url.split("swagger/", 1)[0],
+        'cromwell_server': proxy_url.split("swagger/", 1)[0] if proxy_url else "",
         'requests_timeout': 5,
         'gcloud_token_email': env['user_email'],
-        'referer_header_url': env['referer']
+        'referer_header_url': env['leonardo_url']
      }
      with open(file, 'w') as filetowrite:
         filetowrite.write(json.dumps(configuration, indent=2))
@@ -75,13 +76,13 @@ def find_app_status(env):
     print(f'Checking status for CROMWELL app')
     app_name, app_status, proxy_url = check_for_app(env)
 
-    print(f'app_name={app_name}; app_status={app_status}')
+    configure_cromwell(env, proxy_url)
 
     if app_name is None:
         print(f'CROMWELL app does not exist. Please create cromwell server from workbench')
     else:
+        print(f'app_name={app_name}; app_status={app_status}')
         print(f'Existing CROMWELL app found (app_name={app_name}; app_status={app_status}).')
-        configure_cromwell(env, proxy_url)
         exit(1)
 
 def main():
@@ -93,8 +94,7 @@ def main():
         'user_email': os.environ.get('PET_SA_EMAIL', default = os.environ['OWNER_EMAIL']),
         'owner_email': os.environ['OWNER_EMAIL'],
         'google_project': os.environ['GOOGLE_PROJECT'],
-        'leonardo_url': os.environ['LEONARDO_BASE_URL'],
-        'referer': os.environ['AOU_REFERER']
+        'leonardo_url': os.environ['LEONARDO_BASE_URL']
     }
 
     # Before going any further, check that cromshell2 is installed:
